fbbcadbdda328e1a021b9c661a81f85d11bec5d9
Welnyr <thekotech@gmail.com>
2026-06-18 18:49:14 +0500

    feat: Separate pages logically

---
+++ src/components/ui/UiView.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="ui-view">
+		<div class="ui-view__column" v-bind="$attrs">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.ui-view {
+	display: flex;
+	flex-direction: center;
+	align-items: center;
+	justify-content: center;
+
+	overflow: auto;
+
+	max-block-size: 100%;
+	min-block-size: 100%;
+	block-size: 100%;
+
+	max-inline-size: 100%;
+	min-inline-size: 100%;
+	inline-size: 100%;
+}
+
+.ui-view__column {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	max-inline-size: 700px;
+	min-inline-size: 320px;
+	inline-size: 100%;
+
+	padding-block: 2rem;
+}
+</style>
--- src/views/Players/PlayersView.vue
+++ src/views/Players/PlayersView.vue
@@ -1,3 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UiView from '../../components/ui/UiView.vue'
+</script>
 
-<template>В работе</template>
+<template>
+	<UiView>
+		<h1>Игроки</h1>
+
+		<p>В работе...</p>
+	</UiView>
+</template>
+
+<style scoped>
+h1 {
+	margin-block-end: 1rem;
+}
+
+p {
+	margin-block-end: 10rem;
+}
+</style>
